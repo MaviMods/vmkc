edbc20653413b4383b087bc09053e166116b8e1e
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2025-08-23 21:36:41 +0530

    Create TwitterRedirect.js

---
+++ src/redirect/TwitterRedirect.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+const TwitterRedirect = () => {
+  useEffect(() => {
+    window.location.href = "https://x.com/VMKC_OFFICIAL?t=NtezVFELW1MhqtKZqL5sWw&s=09";
+  }, []);
+
+  return null; // Or a loading spinner
+};
+
+export default TwitterRedirect;
